722ebe54f5cbb99e9bb82472571e3a0f19b58759
GnP <g.parrondo@gmail.com>
2025-02-05 17:51:48 -0300

    Add support to load verifiable binaries

--- src/ethproto/build_artifacts.py
+++ src/ethproto/build_artifacts.py
@@ -6,9 +6,13 @@ import os.path
 import re
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Union, Tuple
+from typing import Tuple, Union
 
 LIBRARY_PLACEHOLDER_MATCHER = re.compile(r"__\$[0-9a-f]{34}\$__")
+
+CONTRACT_REF_MATCHER = re.compile(r"^(?:(?P<package>.*)/)?(?P<contract>[^@]+)(?:@(?P<version>.+))?$")
+
+VERSION_MATCHER = re.compile(r"^\d+\.\d+\.\d+$")
 
 
 @dataclass
@@ -94,6 +98,7 @@ class ArtifactLibrary:
         self.lookup_paths = [Path(p).absolute() for p in paths]
         self._fullpath_cache = {}
         self._name_cache = {}
+        self._ref_cache = None
 
     def get_artifact(self, contract: str) -> Artifact:
         """Returns a build artifact by full contract path
@@ -139,3 +144,115 @@ class ArtifactLibrary:
                 raise FileNotFoundError(f"Could not find artifact for {contract_name} on {self.lookup_paths}")
 
         return self._name_cache[contract_name]
+
+    def _load_ref_cache(self):
+        self._ref_cache = {}
+        for path in self.lookup_paths:
+            path = Path(path)
+            json_files = path.rglob("*.json")
+
+            for json_file in json_files:
+                # Skip build-info and debug files
+                if json_file.parent.stem == "build-info" or json_file.stem.endswith(".dbg"):
+                    continue
+
+                parts = json_file.parts[len(path.parts) - 1 :]
+
+                # Find the first parent named either "build" or "artifacts"
+                try:
+                    build_idx = next(i for i, part in enumerate(parts) if part in ("build", "artifacts"))
+                except StopIteration:
+                    continue
+
+                if build_idx == 0:
+                    # Our lookup path is already a build directory, we're probably looking at a hardhat build output
+                    version = "local"
+                else:
+                    # If we have a version number right before the build directory, we're probably looking at a
+                    # verifiable binaries directory
+                    version = parts[build_idx - 1]
+                    if not VERSION_MATCHER.match(version):
+                        version = "local"
+
+                if version == "local":
+                    # For hardhat output dir, package is everything up to "contracts"
+                    try:
+                        package = "/".join(parts[build_idx + 1 : parts.index("contracts")])
+                    except ValueError:
+                        # There are exceptions, like @openzeppelin/contracts-upgradeable
+                        # For those the package is everythin up to the contract directory
+                        package = "/".join(parts[build_idx + 1 : -2])
+
+                else:
+                    # For verifiable binaries dir, package is everything up to version
+                    package = "/".join(parts[1 : build_idx - 1])
+
+                if json_file.stem not in self._ref_cache:
+                    self._ref_cache[json_file.stem] = []
+                self._ref_cache[json_file.stem].append(
+                    {
+                        "path": json_file,
+                        "package": package,
+                        "version": version,
+                    }
+                )
+
+    def _find_ref(self, contract_ref: str):
+        if self._ref_cache is None:
+            self._load_ref_cache()
+
+        ref_match = CONTRACT_REF_MATCHER.match(contract_ref)
+        if not ref_match:
+            raise ValueError(f"Invalid contract reference: {contract_ref}")
+
+        contract = ref_match.group("contract")
+        if contract not in self._ref_cache:
+            return None
+
+        ref = self._ref_cache[contract]
+
+        package = ref_match.group("package")
+        if package:
+            ref = [r for r in ref if r["package"] == package]
+
+        version = ref_match.group("version")
+        if version:
+            ref = [r for r in ref if r["version"] == version]
+
+        if not ref:
+            return None
+
+        # Sort by version ascending, forcing local to be last
+        ref = sorted(
+            ref,
+            key=lambda x: (
+                x["version"] == "local",
+                tuple(map(int, x["version"].split("."))) if x["version"] != "local" else None,
+            ),
+        )[-1]
+
+        return ref
+
+    def get_artifact_by_ref(self, contract_ref: str) -> Artifact:
+        """Returns a build artifact by looking for a matching contract reference.
+
+        This is compatible with the verifiable binaries structure.
+
+        Accepts the following kind of references:
+
+        - <ContractClass>
+        - <ContractClass>@<version>
+        - <package>/<ContractClass>
+        - <package>/<ContractClass>@<version>
+
+        If version is not specified it uses the latest version available.
+
+        Calling with contract_ref <ContractClass>@local is the same as calling get_artifact_by_name(<ContractClass>).
+        """
+        ref = self._find_ref(contract_ref)
+
+        if ref is None:
+            raise FileNotFoundError(f"Could not find artifact for {contract_ref} on {self.lookup_paths}")
+
+        with open(ref["path"]) as f:
+            return Artifact(**json.load(f))
